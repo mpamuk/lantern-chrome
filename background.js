31bbdd2181fe7b0758463c31617d655fb8cbf5a4
mujde <mujdepamuk@gmail.com>
2015-11-01 17:01:49 -0500

    fblogin pass through

--- background.js
+++ background.js
@@ -3,8 +3,9 @@ var successURL = 'https://www.facebook.com/connect/login_success.html';
 successURL = 'http://localhost:3000/?#';
 function onFacebookLogin() {
                 console.log("ON FB LOGIN");
+                var firstTime = true;
                 console.log(localStorage.accessToken);
-                if (localStorage.accessToken == undefined) {
+                if (firstTime || localStorage.accessToken == undefined) {
                   //http://localhost:3000/?#access_token=CAAM0syS2XLwBAC9bnEjHVOpN3vnhtKeLvNGkq…9KHSAgypkRiVZAUTsTxbfr0ZAX3aaOD5aFxF0ZAcThTsSzubDTnKYZD&expires_in=5174327
                     console.log("COMING TO NULL ACCESS TOKEN");
                     chrome.tabs.getAllInWindow(null, function(tabs) {
